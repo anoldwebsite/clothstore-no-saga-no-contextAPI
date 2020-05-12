f7c187578b46123d76835f5e36d0049c53d1245c
Emil Jan <anoldwebsite@gmail.com>
2020-05-12 14:56:05 +0100

    Container pattern applied to ShopPage component

---
+++ src/components/collections-overview/collections-overview.container.jsx
@@ -0,0 +1,23 @@
+import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
+import {compose} from 'redux';
+import WithSpinner from '../with-spinner/with-spinner.component';
+import CollectionsOverview from './collections-overview.component';
+import {selectIsCollectionFetching} from '../../redux/shop/shop.selectors';
+
+const mapStateToProps = createStructuredSelector(
+    {
+        isLoading: selectIsCollectionFetching
+    }
+);
+
+const CollectionsOverviewContainer = compose(
+    connect(mapStateToProps),
+    WithSpinner
+)(CollectionsOverview);
+/*
+The function above is equivalent to the function below:
+const CollectionsOverviewContainer = connect(mapStateToProps)(WithSpinner(CollectionsOverview))
+*/
+
+export default CollectionsOverviewContainer;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,55 +1,37 @@
 import React from "react";
-import CollectionOverview from "../../components/collections-overview/collections-overview.component";
 import { Route } from "react-router-dom";
-import CollectionPage from "../collection/collection.component";
 import { connect } from "react-redux";
-import WithSpinner from "../../components/with-spinner/with-spinner.component";
 import { fetchCollectionsStartAsync as unboundImportedFetchFollectionStartAsync} from '../../redux/shop/shop.actions';
-import {selectIsCollectionFetching, selectIsCollectionsLoaded} from '../../redux/shop/shop.selectors';
-import {createStructuredSelector} from 'reselect';
-
-
-const CollectionsOverviewWithSpinner = WithSpinner(CollectionOverview);
-const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
+import CollectionPageContainer from '../collection/collection.container';
 
 class ShopPage extends React.Component {
 
   componentDidMount() {
     const { fetchCollectionsStartAsync } = this.props;
-    fetchCollectionsStartAsync();//We call this function to fetch collections asynchronously after the component ShopPage mounting completes.
+    fetchCollectionsStartAsync();//We call this function to fetch collections from Firebase database, asynchronously after the component ShopPage mounting completes.
   }
 
   render() {
-    const { match, isCollectionFetching, isCollectionsLoaded } = this.props;
+    const { match } = this.props;
     return (
       <div className="shop-page">
         <Route
           exact
           path={`${match.path}`} // this gives /shop
-          render={(props) => (
-            <CollectionsOverviewWithSpinner isLoading={isCollectionFetching} {...props} />
-          )}
+          component={CollectionsOverviewContainer}
         />
         <Route
           path={`${match.path}/:collectionId`} //e.g. /shop/hats
-          render={(props) => (
-            <CollectionPageWithSpinner isLoading={!isCollectionsLoaded} {...props} />
-          )}
+          component={CollectionPageContainer}
         />
       </div>
     );
   }
 }
 
-const mapStateToProps = createStructuredSelector(
-  {
-    isCollectionFetching: selectIsCollectionFetching, //Possiblev values are true or false i.e. collections are fetched or not
-    isCollectionsLoaded: selectIsCollectionsLoaded
-  }
-);
-
 const mapDispatchToProps = (dispatch) => ({
   fetchCollectionsStartAsync: () => dispatch(unboundImportedFetchFollectionStartAsync())
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(ShopPage);
+export default connect(null, mapDispatchToProps)(ShopPage);
